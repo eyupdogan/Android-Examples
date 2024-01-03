c579e34c8d5b8c982fb0e69804d50dbdbbf9e4e4
Eyup Dogan <eyodohan@gmail.com>
2024-01-03 19:23:46 +0300

    update

--- src/Projects/Android/WikiSearchRepeat/RepositoryLib/build.gradle.kts
+++ src/Projects/Android/WikiSearchRepeat/RepositoryLib/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
 
-    implementation("org.csystem.android:org-csystem-android-app-retrofit:3.0.0")
+//    implementation("org.csystem.android:org-csystem-android-app-retrofit:3.0.1")
 }
 
 kapt {
