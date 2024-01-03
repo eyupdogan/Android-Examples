5a55b86c1f8ecfce1dd057866f3601419e565483
Eyup Dogan <eyodohan@gmail.com>
2024-01-03 19:06:31 +0300

    update

---
+++ src/Libraries/CSDLibraries/org-csystem-android-app-retrofit/src/main/java/org/csystem/android/app/org_csystem_android_app_retrofit/di/module/RetrofitInterceptor.kt
@@ -0,0 +1,7 @@
+package org.csystem.android.app.org_csystem_android_app_retrofit.di.module
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class RetrofitInterceptor
--- src/Libraries/CSDLibraries/org-csystem-android-app-retrofit/src/main/java/org/csystem/android/app/org_csystem_android_app_retrofit/di/module/RetrofitModule.kt
+++ src/Libraries/CSDLibraries/org-csystem-android-app-retrofit/src/main/java/org/csystem/android/app/org_csystem_android_app_retrofit/di/module/RetrofitModule.kt
@@ -10,12 +10,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-//mvn install:install-file -DgroupId=org.csystem.android -DartifactId=org-csystem-android-datetime -Dversion=20.0.0 -Dfile=../jars/org-csystem-android-util-datetime-release-20.0.0.aar -Dpackaging=aar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
-//mvn install:install-file -DgroupId=org.csystem.android -DartifactId=org-csystem-android-datetime -Dversion=20.0.0 -Dfile=../jars/org-csystem-android-util-datetime-release-20.0.0.aar -Dpackaging=aar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
-//mvn install:install-file -DgroupId=com.dogandev -DartifactId=com-dogandev-util-console -Dversion=11.0.0 -Dfile=../jars/com-dogandev-util-console-11.0.0.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
-
-
-
 @Module
 @InstallIn(SingletonComponent::class)
 object RetrofitModule
