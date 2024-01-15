3c4604e5c2ebec7de229d8a3d454733ee97e4494
Eyup Dogan <eyodohan@gmail.com>
2024-01-16 01:10:34 +0300

    update

--- src/Projects/Android/GeonamesAppMessengerService/WikiSearchMessengerService/app/src/main/java/org/csystem/android/app/service/geonames/search/WikiSearchMessengerService.kt
+++ src/Projects/Android/GeonamesAppMessengerService/WikiSearchMessengerService/app/src/main/java/org/csystem/android/app/service/geonames/search/WikiSearchMessengerService.kt
@@ -2,7 +2,11 @@ package org.csystem.android.app.service.geonames.search
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
+import android.os.Message
+import android.os.Messenger
 import android.widget.Toast
 import dagger.hilt.android.AndroidEntryPoint
 import org.csystem.android.app.service.geonames.search.api.IGeonamesWikiSearchService
@@ -10,13 +14,18 @@ import org.csystem.android.app.service.geonames.search.api.WikiSearch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.ref.WeakReference
 import java.time.format.DateTimeFormatter
 import java.util.concurrent.ExecutorService
 import javax.inject.Inject
 
+const val WHAT_WIKI_SEARCH = 0
+
 @AndroidEntryPoint
 class WikiSearchMessengerService : Service()
 {
+    private lateinit var mMessenger:Messenger
+    private lateinit var mHandler:Handler
     @Inject
     lateinit var wikiSearchService: IGeonamesWikiSearchService
 
@@ -26,32 +35,50 @@ class WikiSearchMessengerService : Service()
     @Inject
     lateinit var threadPool:ExecutorService
 
+    private class WikiSearchHandler(service:WikiSearchMessengerService):Handler(Looper.myLooper()!!)
+    {
+        private val mWeakReference = WeakReference(service)
+
+        private fun wikiSearchCallback(text:String, maxRows:Int)
+        {
+            val service = mWeakReference.get()!!
+            val wikiService = service.wikiSearchService
+            val call = wikiService.findByQ("zonguldak", 10, "csystem")
+
+            call.enqueue(object : retrofit2.Callback<WikiSearch>
+            {
+                override fun onResponse(call: Call<WikiSearch>, response: Response<WikiSearch>)
+                {
+                    val wikiSearch = response.body()
+
+                    if (wikiSearch?.wikiInfo != null){
+                        wikiSearch.wikiInfo.forEach { Toast.makeText(service, it.summary, Toast.LENGTH_SHORT).show() }
+                    }else
+                        Toast.makeText(service, "Error in geonames service", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onFailure(call: Call<WikiSearch>, ex: Throwable)
+                {
+                    Toast.makeText(service, "Exception occurred:${ex.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+        override fun handleMessage(msg: Message)
+        {
+            when(msg.what){
+                WHAT_WIKI_SEARCH -> wikiSearchCallback(msg.obj.toString(), msg.arg1)
+            }
+            super.handleMessage(msg)
+        }
+    }
     
     override fun onBind(intent: Intent): IBinder
     {
+        Toast.makeText(this, "client bounded", Toast.LENGTH_SHORT).show()
+        mHandler = WikiSearchHandler(this)
+        mMessenger = Messenger(mHandler)
 
-        TODO("Return the communication channel to the service.")
-    }
-
-    fun getButtonClicked()
-    {
-        val call = wikiSearchService.findByQ("zonguldak", 10, "csystem")
-
-        call.enqueue(object :Callback<WikiSearch>{
-            override fun onResponse(call: Call<WikiSearch>, response: Response<WikiSearch>)
-            {
-                val wikiSearch = response.body()
-
-                if (wikiSearch?.wikiInfo != null){
-                    wikiSearch.wikiInfo.forEach {  }
-                }else
-                    Toast.makeText(this@WikiSearchMessengerService, "Error in geonames service", Toast.LENGTH_LONG).show()
-            }
-
-            override fun onFailure(call: Call<WikiSearch>, ex: Throwable)
-            {
-                Toast.makeText(this@WikiSearchMessengerService, "Exception occurred:${ex.message}", Toast.LENGTH_LONG).show()
-            }
-        })
+        return mMessenger.binder
     }
 }
