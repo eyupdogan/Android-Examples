e73b72eb8053e2c45de2b90f26a80ee36c3059e8
Eyup Dogan <eyodohan@gmail.com>
2024-01-19 18:16:51 +0300

    update

--- src/Projects/Android/PaymentSystem/PaymentAndroidApplication/RepositoryLib/src/main/java/org/csystem/android/app/payment/repository/UserRepository.kt
+++ src/Projects/Android/PaymentSystem/PaymentAndroidApplication/RepositoryLib/src/main/java/org/csystem/android/app/payment/repository/UserRepository.kt
@@ -1,28 +1,64 @@
+/*--------------------------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------*/
 package org.csystem.android.app.payment.repository
 
 import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
 import org.csystem.android.app.payment.repository.dao.IUserRepository
 import org.csystem.android.app.payment.repository.entity.User
+import org.csystem.android.app.payment.repository.global.USER_FILE
+import java.io.EOFException
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 import java.util.Optional
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(@ApplicationContext context: Context):IUserRepository
 {
-    override fun <S : User?> save(entity: S): S
+    private val mContext:Context = context
+    private fun <S:User?> saveCallback(fos:FileOutputStream, user:S):S
     {
-        TODO("Not yet implemented")
+        ObjectOutputStream(fos).writeObject(user)
+        return user
     }
+
+    private fun findByUsernameAndPasswordCallback(fis:FileInputStream, username: String, password: String):User?
+    {
+        var user:User? = null
+        try {
+            while (true){
+                user = ObjectInputStream(fis).readObject() as? User
+                if (user?.username == username && user.password == password)
+                    break
+            }
+        }catch (ignore:EOFException){
+
+        }
+        return user
+    }
+    override fun <S : User?> save(user: S): S
+    {
+        return mContext.openFileOutput(USER_FILE, Context.MODE_APPEND).use { saveCallback(it, user) }
+    }
+
+    override fun findByUsernameAndPassword(username: String, password: String): User?
+    {
+        return mContext.openFileInput(USER_FILE).use { findByUsernameAndPasswordCallback(it, username, password) }
+    }
+
+    ////////////////////////////////////////////////////////////
+
     override fun existsByUsernameAndPassword(username: String, password: String): Boolean
     {
         TODO("Not implemented yet")
     }
 
-    override fun findByUsernameAndPassword(username: String, password: String): User?
-    {
-        TODO("Not yet implemented")
-    }
-////////////////////////////////////////////////////////////
+
+
+
 
     override fun count(): Long
     {
