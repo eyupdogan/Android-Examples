fc76c107a5991bb85173f3582f2e03a241b54a90
Eyup Dogan <eyodohan@gmail.com>
2024-02-21 15:56:41 +0300

    update

--- src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/java/org/csystem/app/app.kt
@@ -1,13 +1,41 @@
-/*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: Parametresi ile aldığı Int türden bir sayının tersini döndüren reversed isimli fonksiyonu yazınız
-    ve aşağıdaki kod ile test ediniz.
-    Algoritma: 123 -> 3 -> 3 * 10 + 2 = 32 -> 32 * 10 + 1 = 321
-----------------------------------------------------------------------------------------------------------------------*/
+
 package org.csystem.app
+
+import kotlin.math.pow
+
 
 fun main()
 {
-    for (value in 10 downTo 1)
-        println(value)
+    println(isPrime(4935060337471977161))
+
 }
 
+fun isPrime(value:Long, n:Int):Boolean
+{
+    if (value % n == 0L)
+        return false
+
+    if (n * n > value)
+        return true
+
+    return isPrime(value, n + 2)
+}
+fun isPrime(value:Long):Boolean
+{
+    if (value % 2 == 0L)
+        return value == 2L
+
+    if (value % 3 == 0L)
+        return value == 3L
+
+    if (value % 5 == 0L)
+        return value == 5L
+
+    if (value % 7 == 0L)
+        return value == 7L
+
+    if (value % 11 == 0L)
+        return value == 11L
+
+    return isPrime(value, 11)
+}
